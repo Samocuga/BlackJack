d0f749188b5b2dfb2d239daea6ef2a21e41b7498
Samuel <Samuelcuellog@gmail.com>
2023-10-10 11:32:36 -0500

    "Creacion de modelos"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { BlogComponent } from './components/blog/blog.component';
 import { PaginaComponent } from './components/pagina/pagina.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { ErrorComponent } from './components/error/error.component';
+import { Pagina2Component } from './components/pagina2/pagina2.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'home', component: HomeComponent },
   {path: 'blog', component: BlogComponent },
+  {path: 'pagina', component: PaginaComponent },
   {path: 'pagina/:nombre', component: PaginaComponent },
+  {path: 'pagina2', component: Pagina2Component},
   {path: 'formulario', component: FormularioComponent },
   {path: '**', component: ErrorComponent}
 ];
---
+++ src/app/models/aprendiz.ts
@@ -0,0 +1,11 @@
+export class Aprendiz {
+  public nombre: string;
+  public apellido: string;
+  public edad: number;
+
+  constructor(nombre: string, apellido: string, edad: number) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+  }
+}
